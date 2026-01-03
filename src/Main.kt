85bb376a05d9f857eceb84be848603a7643c8af2
01217 <andreas.adlesgruber@gmx.at>
2026-01-03 09:52:23 +0100

    Create Project structure
    This patch creates the project structure for day 10 of advent of code 2025, written in kotlin:latest

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val resultTaskOne = taskOne("inp/input.in")
+    val resultTaskTwo = taskTwo("inp/input.in")
+
+    println("Task1: $resultTaskOne")
+    println("Task2: $resultTaskTwo")
+}
+
+fun taskOne(filePath : String) : Long  {
+    return 0L
+}
+
+fun taskTwo(filePath : String) : Long {
+    return 0L
+}
---
+++ tst/Test.kt
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class Test {
+
+    @Test
+    @DisplayName("task one test")
+    fun taskOneTest() {
+        val expectedResult = 0L
+
+        Assertions.assertEquals(expectedResult, taskOne("inp/test.in"));
+    }
+
+    @Test
+    @DisplayName("task two test")
+    fun taskTwoTest() {
+        val expectedResult = 0L
+
+        Assertions.assertEquals(expectedResult, taskTwo("inp/test.inp"));
+    }
+}
